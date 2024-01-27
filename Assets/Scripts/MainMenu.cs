6ce5c104298a1b32503bf4700c28ca627651069c
Joel Bernardes <119889298+JoelBernardes@users.noreply.github.com>
2024-01-27 14:58:17 -0500

    Added more functionality to the win screen
    It has the funny now

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,15 +6,39 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject Text;
+    [SerializeField]
+    private GameObject hiddenScene;
+
+    private float next = 1f;
+    private bool fakeMainMenu = false;
     void Start()
     {
-        
+        Text.SetActive(true);
+        hiddenScene.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (fakeMainMenu)
+        {
+            Text.SetActive(false);
+            hiddenScene.SetActive(true);
+            if (next <= 0f)
+            {
+                Quit();
+            }
+            else
+            {
+                next -= Time.deltaTime;
+            }
+        }
+        else
+        {
+            Debug.Log("Working...");
+        }
     }
 
     public void Replay()
@@ -25,5 +49,15 @@ public class MainMenu : MonoBehaviour
     public void Home()
     {
         SceneManager.LoadScene("Sample Scene"); //should send you back to main menu
+    }
+
+    public void fakeHome()
+    {
+        fakeMainMenu = true;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
